b95c2e6c881dd30f48df831e897e75e5d0d3526c
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-07-26 12:34:39 -0500

    MVP but Barely

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
-   {/*https://github.com/Bryce-Soghigian/Sprint-Challenge-State-Management-Smurfs.git */}
+
+import React, { Component } from 'react';
+import './App.css';
+import SmurfVillage from './SmurfVillage'
+/*
+ to wire this component up you're going to need a few things.
+ I'll let you do this part on your own. 
+ Just remember, `how do I `connect` my components to redux?`
+ `How do I ensure that my component links the state to props?`
+ */
 class App extends Component {
   render() {
     return (
-   
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
+    
         <h1>Hey sister smurf</h1>
         <h1>Hey brother Smurf How the Village be?</h1>
+        <h1>Idk check the village</h1>
+       <SmurfVillage/>
+        
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
